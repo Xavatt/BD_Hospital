04348f670f6f81fbee4b459fe3ceacea2a24e1f5
Xavier Guinto <xavierguinto3@outlook.es>
2018-03-23 09:39:01 -0600

    trabajo rama

--- BD_Hospital.java
+++ BD_Hospital.java
@@ -5,7 +5,7 @@ public class BD_Hospital
 
     public static void main(String[] args)
     {
-        System.out.println("GitHub nueva rama")
+        System.out.println("GitHub nueva rama");
     }
 
 }
