d3ce37a458c377f2fb417bd33b33e5b1458100c1
Xavier Guinto <xavierguinto3@outlook.es>
2018-04-03 14:27:19 -0500

    Paciente DP codigo

---
+++ PacienteDP.java
@@ -0,0 +1,71 @@
+package bd_hospital;
+
+
+import java.util.StringTokenizer;
+
+public class PacienteDP {
+    private String clave, nombre, direccion, telefono;
+
+    public PacienteDP()
+    {
+        this.clave = "";
+        this.nombre = "";
+        this.direccion = "";
+    }
+    
+    public PacienteDP(String datos)
+    {
+        StringTokenizer st = new StringTokenizer(datos, "_");
+        clave = st.nextToken();
+        nombre = st.nextToken();
+        direccion = st.nextToken();
+        telefono = st.nextToken();
+        
+    }
+    
+    
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() 
+    {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) 
+    {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() 
+    {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) 
+    {
+        this.telefono = telefono;
+    }    
+    
+    public String toStringSql(){
+    	return "'"+this.clave + "','" + this.nombre + "'," + this.direccion + "," + this.telefono+"'";
+    }
+    
+    public String toString(){
+    	return this.clave + "_" + this.nombre + "_" + this.direccion + "_" + this.telefono;
+    }
+}
