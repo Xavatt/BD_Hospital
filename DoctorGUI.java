0556c6b5fe038cadd90c3233834efd8e3e86b4f3
Xavier Guinto <xavierguinto3@outlook.es>
2018-04-03 12:20:19 -0500

    Se agrego atributos

--- DoctorGUI.java
+++ DoctorGUI.java
@@ -3,5 +3,11 @@ package bd_hospital;
 
 public class DoctorGUI extends JFrame implements ActionListener
 {
-
+    /**
+    * Definir atributos que se van a ocupar
+    */
+    private JTextField tfNocta, tfNombre, tfTipo, tfSaldo, tfFecha, tfHora, tfFechaApertura;
+    private JButton bCapturar, bConsultar, bConsultarCve, bConsultarEspecialidad, bSalir;
+    private JPanel panel1, panel2;
+    private JTextArea taDatos;
 }
