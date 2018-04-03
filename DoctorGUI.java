25a7fbdea99831317fbac30d18e078af05399b6d
Xavier Guinto <xavierguinto3@outlook.es>
2018-04-03 12:23:18 -0500

    Version 3.5

--- DoctorGUI.java
+++ DoctorGUI.java
@@ -6,8 +6,13 @@ public class DoctorGUI extends JFrame implements ActionListener
     /**
     * Definir atributos que se van a ocupar
     */
-    private JTextField tfNocta, tfNombre, tfTipo, tfSaldo, tfFecha, tfHora, tfFechaApertura;
+    private JTextField tfClave, tfNombre, tfEspecialidad, tfDireccion, tfTelefono;
     private JButton bCapturar, bConsultar, bConsultarCve, bConsultarEspecialidad, bSalir;
     private JPanel panel1, panel2;
     private JTextArea taDatos;
+
+    public Doctor()
+    {
+      
+    }
 }
