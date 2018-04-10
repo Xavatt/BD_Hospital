88510da82789ca935906c05d1c156f53315dced3
Xavier Guinto <xavierguinto3@outlook.es>
2018-04-09 21:49:16 -0500

    Modificaciones

--- HospitalGUI.java
+++ HospitalGUI.java
@@ -2,10 +2,10 @@
  * @(#)HospitalGUI.java
  *
  *
- * @author 
+ * @author
  * @version 1.00 2018/4/3
  */
- 
+
 package bd_hospital;
 
 import javax.swing.JFrame;
@@ -26,61 +26,61 @@ public class HospitalGUI extends JFrame implements ActionListener
 	private JMenu		menuAdministracion, menuDoctor, menuPaciente, menuAnalisis;
 	private JMenuItem	miDoctor, miAtiende, miPaciente, miAnalisis, miSalir;
 	private JPanel		panel;
-	
+
 	private DoctorGUI	doctor		= new DoctorGUI();
 	private PacienteGUI	paciente	= new PacienteGUI();
 	private AtiendeGUI	atiende		= new AtiendeGUI();
 	private AnalisisGUI	analisis	= new AnalisisGUI();
-	
+
     public HospitalGUI()
     {
     	super("Sistema de BD Hospital");
-    	
+
     	mbProyecto 		= new JMenuBar();
-    	
-    	menuAdministracion	= new JMenu("Administracion del Hospital");
-    	menuDoctor			= new JMenu("Doctores");
-    	menuPaciente		= new JMenu("Pacientes");
-    	menuAnalisis		= new JMenu("Analisis");
-    	
+
+    	menuAdministracion	= new JMenu("ADMINISTRACION DEL HOSPITAL");
+    	menuDoctor			= new JMenu("DOCTORES");
+    	menuPaciente		= new JMenu("PACIENTES");
+    	menuAnalisis		= new JMenu("ANALISIS CLINICOS");
+
     	miDoctor			= new JMenuItem("Altas y consultas de Doctores");
     	miAtiende			= new JMenuItem("Asignacion de Doctor a Paciente");
     	miPaciente			= new JMenuItem("Altas y consultas de Pacientes");
     	miAnalisis			= new JMenuItem("Catalogo de Analisis");
-    	
+
     	miSalir				= new JMenuItem("EXIT");
-    	
+
     	panel				= new JPanel();
-    	
+
     	miDoctor.addActionListener(this);
     	miAtiende.addActionListener(this);
     	miPaciente.addActionListener(this);
     	miAnalisis.addActionListener(this);
-    	
+
     	miSalir.addActionListener(this);
-    	
+
     	menuDoctor.add(miDoctor);
     	menuDoctor.add(miAtiende);
     	menuPaciente.add(miPaciente);
     	menuAnalisis.add(miAnalisis);
-    	
+
     	menuAdministracion.add(menuDoctor);
     	menuAdministracion.add(menuPaciente);
     	menuAdministracion.add(menuAnalisis);
-    	
+
     	mbProyecto.add(menuAdministracion);
     	mbProyecto.add(miSalir);
-    	
+
     	setJMenuBar(mbProyecto);
 		setSize(500,510);
 		setVisible(true);
     }
-    
+
     private void ocultarPanel()
 	{
 		panel.setVisible(false);
 	}
-	
+
     public void actionPerformed(ActionEvent evento)
     {
     	if(evento.getSource() == miDoctor)
@@ -89,47 +89,48 @@ public class HospitalGUI extends JFrame implements ActionListener
 			panel = doctor.getPanel();
 			panel.setVisible(true);
 			add(panel);
-			
+
 			setVisible(true);
 		}
-		
+
 		if(evento.getSource() == miAtiende)
 		{
 			ocultarPanel();
 			panel= atiende.getPanel();
 			panel.setVisible(true);
 			add(panel);
-			
+
 			setVisible(true);
 		}
-		
+
 		if(evento.getSource() == miPaciente)
 		{
 			ocultarPanel();
 			panel= paciente.getPanel();
 			panel.setVisible(true);
 			add(panel);
-			
+
 			setVisible(true);
 		}
-		
+
 		if(evento.getSource() == miAnalisis)
 		{
 			ocultarPanel();
 			panel= analisis.getPanel();
 			panel.setVisible(true);
 			add(panel);
-			
+
 			setVisible(true);
 		}
 		if(evento.getSource() == miSalir)
 		{
 			System.exit(0);
 		}
+
     }
-    
+
     public static void main(String args[])
     {
-    	HospitalGUI objeto = new HospitalGUI();
+    	HospitalGUI hospital = new HospitalGUI();
     }
 }
